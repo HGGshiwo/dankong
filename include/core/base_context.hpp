b5770e23fb4eda30092311a8599e7867e443b322
HGGshiwo <3190102788@zju.edu.cn>
2026-06-25 15:59:29 +0800

    修改cmakelist

--- include/core/base_context.hpp
+++ include/core/base_context.hpp
@@ -8,10 +8,6 @@
 #include "robot/irobot.hpp"
 #include "utils/state_registry.hpp"
 
-namespace mavsdk {
-class System;
-}
-
 // 所有机器人 Context 共享的基础数据结构
 struct IContext {
     virtual ~IContext() = default;
